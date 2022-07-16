5b5e32ef12c8f8e2460e085c86a015300163e823
Lambda <34376990+LambdaTheDev@users.noreply.github.com>
2022-07-16 20:53:35 +0200

    Added prop type switch feature + bumped version

---
+++ AsmResolverKit/PropertyHelper.cs
@@ -0,0 +1,25 @@
+﻿using AsmResolver.DotNet;
+using AsmResolver.DotNet.Signatures.Types;
+
+namespace LambdaTheDev.AsmResolverKit
+{
+    // Helper methods for properties
+    public static class PropertyHelper
+    {
+        // This method changes property return type
+        public static void ChangePropertyType(PropertyDefinition property, TypeSignature newType)
+        {
+            if (property.Signature == null) return;
+            
+            // Update property return type
+            property.Signature.ReturnType = newType;
+            
+            // Update getter & setter if present
+            if (property.GetMethod != null)
+                property.GetMethod.Signature!.ReturnType = newType;
+
+            if (property.SetMethod != null)
+                property.SetMethod.Signature!.ParameterTypes[0] = newType;
+        }
+    }
+}
