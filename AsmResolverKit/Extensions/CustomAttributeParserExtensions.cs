50873beb9b3f07dec3960e7fb7f252980b7d4e19
Lambda <34376990+LambdaTheDev@users.noreply.github.com>
2022-07-16 20:02:05 +0200

    Implemented basics

---
+++ AsmResolverKit/Extensions/CustomAttributeParserExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Reflection;
+using AsmResolver;
+using AsmResolver.DotNet;
+
+namespace LambdaTheDev.AsmResolverKit.Extensions
+{
+    // This class allows you to parse CustomAttribute into a provided model (with the same properties name)
+    public static class CustomAttributeParserExtensions
+    {
+        public static T? Parse<T>(this CustomAttribute attribute) where T : class, new()
+        {
+            // Prepare result data
+            T result = new T();
+            Type resultType = typeof(T);
+
+            if (attribute.Signature == null) return null;
+        
+            // Loop through argument properties & find result properties that match
+            foreach (var attributeArg in attribute.Signature.NamedArguments)
+            {
+                PropertyInfo? resultProperty = resultType.GetProperty(attributeArg.MemberName?.Value!);
+                if(resultProperty == null) continue;
+
+                // String param is parsed to UTF8 string by AsmResolver. Here I fix this issue
+                object? propertyValue = attributeArg.Argument.Element;
+                if (propertyValue != null && propertyValue is Utf8String utf8String)
+                    propertyValue = utf8String.Value;
+            
+                // Set value
+                resultProperty.SetValue(result, propertyValue);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ AsmResolverKit/Extensions/HasCustomAttributeExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using AsmResolver.DotNet;
+
+namespace LambdaTheDev.AsmResolverKit.Extensions
+{
+    // Extension methods for custom attribute providers
+    public static class HasCustomAttributeExtensions
+    {
+        // This method returns parsed CustomAttribute, or null if attribute is not present
+        public static T? FindAndParseCustomAttribute<T>(this IHasCustomAttribute provider, string attrNamespace, string attrName) where T : class, new()
+        {
+            // Try obtain CustomAttribute
+            CustomAttribute? attribute = provider.FindCustomAttributes(attrNamespace, attrName).FirstOrDefault();
+            if (attribute == null) return null;
+
+            // & parse it
+            return attribute.Parse<T>();
+        }
+    }
+}
